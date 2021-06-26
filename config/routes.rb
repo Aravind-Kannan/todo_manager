7ecf7aae30684f805d9c75966a494eafe0ed21cf
aravind <aravindkannan2001@gmail.com>
2021-06-26 19:57:23 +0530

    Can see a single todo in /todo/:id

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  # maps "todos" endpoint  to "todos#index" = TodosController with index action(method)
   get "todos", to: "todos#index"
+
+  # maps "todos/:id" endpoint  to "todos#show" = TodosController with index show(method)
+  # params[:id] - Hash containing Path Params of exposed endpoint
+  get "todos/:id", to: "todos#show"
 end
