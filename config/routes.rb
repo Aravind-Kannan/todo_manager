b13bed5bc7d2dd700ea3a2eb3cf49ab01bbc7075
aravind <aravindkannan2001@gmail.com>
2021-06-26 22:16:29 +0530

    Use the resources routing convention for Todos

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
-  # maps "todos" endpoint  to "todos#index" = TodosController with index action(method)
-  get "todos", to: "todos#index"
+  ## maps "todos" endpoint  to "todos#index" = TodosController with index action(method)
+  #get "todos", to: "todos#index"
+  #
+  ## maps "todos/create" endpoint  to "todos#create" = TodosController with create action(method)
+  ## Query params - follows ?todo_text="<fill>"&due_date="<fill>
+  #post "todos", to: "todos#create"
+  #
+  ## maps "todos/:id" endpoint  to "todos#show" = TodosController with index show(method)
+  ## params[:id] - Hash containing (here, Path) Params of exposed endpoint
+  #get "todos/:id", to: "todos#show"
 
-  # maps "todos/create" endpoint  to "todos#create" = TodosController with create action(method)
-  # Query params - follows ?todo_text="<fill>"&due_date="<fill>
-  get "todos/create", to: "todos#create"
-
-  # maps "todos/:id" endpoint  to "todos#show" = TodosController with index show(method)
-  # params[:id] - Hash containing (here, Path) Params of exposed endpoint
-  get "todos/:id", to: "todos#show"
+  resources :todos
 end
