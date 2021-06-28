4e8bc00a55a3ec41d3bbb27202478ff3ac1c1567
aravind <aravindkannan2001@gmail.com>
2021-06-28 13:36:38 +0530

    Added resources :users for user management endpoint

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
+  end
+
+  def show
+    render plain: User.find(params[:id]).to_pleasant_string
+  end
+
+  def create
+    name = params[:name]
+    password = params[:password]
+    email = params[:email]
+    new_user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+    response_text = "Hey! Your user ID is #{new_user.id}"
+    render plain: response_text
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   #get "todos/:id", to: "todos#show"
 
   resources :todos
+  resources :users
 end
