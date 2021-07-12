d0c6dc004f951072958c309f774b08623cc8fda0
aravind <aravindkannan2001@gmail.com>
2021-07-12 20:32:34 +0530

    Clean controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,6 @@ class UsersController < ApplicationController
     render "users/new"
   end
 
-  def index
-    render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
-  end
-
-  def show
-    render plain: User.find(params[:id]).to_pleasant_string
-  end
-
   def create
     user = User.new(
       first_name: params[:first_name],
@@ -27,12 +19,5 @@ class UsersController < ApplicationController
       session[:current_user_id] = user.id
       redirect_to "/"
     end
-  end
-
-  def login
-    email = params[:email]
-    password = params[:password]
-    response_text = User.where("email = ? and password = ?", email, password).first.present?
-    render plain: response_text
   end
 end
